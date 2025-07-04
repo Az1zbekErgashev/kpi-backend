afb4ab3aab7babed35d43cd34d42d3cbcce25e8d
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-07-04 13:56:07 +0900

    fixed some bugs

---
+++ Kpi.Api/Migrations/20250704045559_UPDATE_USER_NAME_ADMIN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kpi.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UPDATE_USER_NAME_ADMIN : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt", "UserName" },
+                values: new object[] { new DateTime(2025, 7, 4, 4, 55, 58, 970, DateTimeKind.Utc).AddTicks(9548), new DateTime(2025, 7, 4, 4, 55, 58, 970, DateTimeKind.Utc).AddTicks(9550), "CEO" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt", "UserName" },
+                values: new object[] { new DateTime(2025, 7, 2, 7, 30, 42, 850, DateTimeKind.Utc).AddTicks(5868), new DateTime(2025, 7, 2, 7, 30, 42, 850, DateTimeKind.Utc).AddTicks(5871), "admin" });
+        }
+    }
+}
